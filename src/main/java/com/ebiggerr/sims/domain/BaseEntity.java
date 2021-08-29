52859296964cdd6586601384d1d1f6f9210f8259
Alexious Yong <alexiousyong@outlook.com>
2021-08-29 13:56:23 +0800

    Enhance - Refactoring and update Account entity
    1. changed baseEntity to BaseEntity (naming conventions)
    2. Account entity

--- src/main/java/com/ebiggerr/sims/domain/BaseEntity.java
+++ src/main/java/com/ebiggerr/sims/domain/BaseEntity.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @MappedSuperclass
-public class baseEntity {
+public class BaseEntity {
 
     @Id
     @GeneratedValue(generator = "UUID")
--- src/main/java/com/ebiggerr/sims/domain/account/Account.java
+++ src/main/java/com/ebiggerr/sims/domain/account/Account.java
@@ -1,35 +1,54 @@
 package com.ebiggerr.sims.domain.account;
 
+import com.ebiggerr.sims.domain.BaseEntity;
+import com.ebiggerr.sims.enumeration.AccountStatus;
+import com.ebiggerr.sims.enumeration.PostgreSQLEnumType;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.Collection;
 import java.util.Set;
-import java.util.UUID;
 
 @Entity
+@TypeDef(
+        name = "pgsql_enum",
+        typeClass = PostgreSQLEnumType.class
+)
 @Table(name="account")
-public class Account implements UserDetails {
+public class Account extends BaseEntity implements UserDetails {
 
-    @Id
-    private final UUID id = UUID.randomUUID();
-
-    private LocalDateTime creationTime;
-
-    private LocalDateTime lastModificationTime;
-
+    @NotEmpty
+    @Column(length = 64)
     private String username;
 
+    @NotEmpty
+    @Column(length=60) // char(60) in postgres for the BCrypt hashing
+    @Size(min = 60)
     private String password;
 
+    @Column(length = 128)
     private String emailAddress;
+
+    @Column(length = 64)
+    private String firstName;
+
+    @Column(length = 64)
+    private String lastName;
 
     private String remarks;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "accountstatus")
+    @Type( type = "pgsql_enum" )
+    private AccountStatus accountStatus;
+
     @OneToMany(fetch=FetchType.EAGER)
-    @JoinColumns({@JoinColumn(name="accountid",referencedColumnName = "accountid")})
+    @JoinColumn(name= "accountId", referencedColumnName = "id", nullable = false)
     private Set<AccountRole> accountRoleSet;
 
     @Override
@@ -39,31 +58,51 @@ public class Account implements UserDetails {
 
     @Override
     public String getPassword() {
-        return null;
+        return this.password;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return this.username;
+    }
+
+    public String getFirstName(){
+        return this.firstName;
+    }
+
+    public String getLastName(){
+        return this.lastName;
+    }
+
+    public String getEmailAddress(){
+        return this.emailAddress;
+    }
+
+    public String getRemarks(){
+        return this.remarks;
+    }
+
+    public AccountStatus getAccountStatus(){
+        return this.accountStatus;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
