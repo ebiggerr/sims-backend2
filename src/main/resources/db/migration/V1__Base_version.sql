747189dea60d118a5fe57fa57aba4e03a3b496b8
Alexious Yong <alexiousyong@outlook.com>
2021-08-29 12:25:09 +0800

    Doc - Initial Migration for FLyway

---
+++ src/main/resources/db/migration/V1__Base_version.sql
@@ -0,0 +1,77 @@
+CREATE TYPE accountstatus AS enum ('APPROVED', 'PENDING', 'REVOKED');
+
+CREATE TABLE public.account (
+    id uuid NOT NULL,
+    username character varying(64),
+    password character(60),
+    "creationTime" timestamp without time zone,
+    "lastModificationTime" timestamp without time zone,
+    "isDeleted" boolean,
+    "firstName" character varying(64),
+    "lastName" character varying(64),
+    "emailAddress" character varying(128),
+    "accountStatus" public.accountstatus DEFAULT 'PENDING'::public.accountstatus,
+    remarks text
+);
+
+CREATE TABLE public."accountRole" (
+    "accountId" uuid NOT NULL,
+    "roleId" character varying(32) NOT NULL,
+    "creationTime" timestamp without time zone,
+    "isDeleted" boolean,
+    "lastModificationTime" timestamp without time zone,
+    username character varying(64)
+);
+
+CREATE TABLE public."roleDetails" (
+    "roleId" character varying(32) NOT NULL,
+    "roleName" character varying(64),
+    "roleDescription" text,
+    "creationTime" timestamp without time zone,
+    "lastModificationTime" timestamp without time zone,
+    "isDeleted" boolean
+);
+
+--
+-- Name: account account_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.account
+    ADD CONSTRAINT account_pk PRIMARY KEY (id);
+
+
+--
+-- Name: accountRole accountrole_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."accountRole"
+    ADD CONSTRAINT accountrole_pk PRIMARY KEY ("accountId", "roleId");
+
+--
+-- Name: roleDetails roledetails_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."roleDetails"
+    ADD CONSTRAINT roledetails_pk PRIMARY KEY ("roleId");
+
+--
+-- Name: roledetails_roleid_uindex; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE UNIQUE INDEX roledetails_roleid_uindex ON public."roleDetails" USING btree ("roleId");
+
+
+--
+-- Name: accountRole accountrole_accountId_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."accountRole"
+    ADD CONSTRAINT "accountrole_accountId_fk" FOREIGN KEY ("accountId") REFERENCES public.account(id);
+
+
+--
+-- Name: accountRole accountrole_roleId_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."accountRole"
+    ADD CONSTRAINT "accountrole_roleId_fk" FOREIGN KEY ("roleId") REFERENCES public."roleDetails"("roleId") ON UPDATE CASCADE;
